63bb659fb10f8a2d3b5209fdb3cae81932b6938c
freire-l <85429031+freire-l@users.noreply.github.com>
2022-09-29 11:24:21 -0500

    Update comments to graphnode.cpp

--- src/graphnode.cpp
+++ src/graphnode.cpp
@@ -8,13 +8,11 @@ GraphNode::GraphNode(int id)
 
 GraphNode::~GraphNode()
 {
-    //// STUDENT CODE
-    ////
+
 
     //delete _chatBot;       //Deleted for task 0
 
-    ////
-    //// EOF STUDENT CODE
+
 }
 
 void GraphNode::AddToken(std::string token)
@@ -32,8 +30,7 @@ void GraphNode::AddEdgeToChildNode(std::unique_ptr<GraphEdge> edge)
     _childEdges.push_back(std::move(edge));
 }
 
-//// STUDENT CODE
-////
+
 //void GraphNode::MoveChatbotHere(ChatBot *chatbot)   //task 5
 void GraphNode::MoveChatbotHere(ChatBot chatbot)
 {
@@ -51,17 +48,14 @@ void GraphNode::MoveChatbotToNewNode(GraphNode *newNode)
     newNode->MoveChatbotHere(_chatBot);
     //_chatBot = nullptr; // invalidate pointer at source //deleted for task 5
 }
-////
-//// EOF STUDENT CODE
+
 
 GraphEdge *GraphNode::GetChildEdgeAtIndex(int index)
 {
-    //// STUDENT CODE
-    ////
+
 
     //return _childEdges[index];     //deleted for task 4
     return _childEdges[index].get();
 
-    ////
-    //// EOF STUDENT CODE
+
 }
